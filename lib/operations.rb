bdd7e801bfca67518de1fa55290e0f45b6d1fe09
Nattida Wong <nattidac.wong@gmail.com>
2020-09-12 01:28:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+  speed == 79
+   if speed > 60
+    "true"
+  else
+    "false"
+  end
 end
 
-
+##
 
 def not_safe?(speed)
-	
+  speed > 79 ? true : false
 end
-	
 
+def not_safe?(speed)
+end
 
+def not_safe?(speed)
+  speed > 50 ? true : false
+end
